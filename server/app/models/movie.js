5d712bca7e991de6a862e390a73fd2f24c064c3c
Preston (desktop) <smith.preston2@gmail.com>
2016-10-26 16:46:25 -0600

    Add movie model

---
+++ server/app/models/movie.js
@@ -0,0 +1,17 @@
+﻿var mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/db_name');
+var db = mongoose.connection;
+
+console.log('Database ' + db.name + 'created');
+
+var movieTemplate = {
+    name: String,
+    released: Boolean,
+    watched: Boolean
+};
+
+var movieSchema = mongoose.Schema(movieTemplate);
+
+console.log('Movie Model created');
+module.exports = mongoose.model('Movie', movieSchema);
---
+++ server/appserver.js
@@ -0,0 +1,29 @@
+﻿var express = require('express');
+var bodyParser = require('body-parser');
+var router = express.Router();
+var Movie = require('./app/models/movie.js');
+var app = express();
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var port = process.env.PORT || 8080;
+
+router.use(function(req, res, next) {
+
+    console.log('Something is happening');
+    next();
+
+});
+
+router.get('/',
+    function (req, res) {
+        res.json({ message: 'hooray! welcome to our API!' });
+});
+
+app.use('/api', router);
+
+//app.listen(port);
+console.log('Magic happens on port ' + port);
+
+
