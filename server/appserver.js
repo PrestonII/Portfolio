caeaf7f67767a70d76e1f1b504326fb9a0232230
Preston (desktop) <smith.preston2@gmail.com>
2016-10-26 22:58:10 -0600

    Add CRUD ops

--- server/appserver.js
+++ server/appserver.js
@@ -21,9 +21,82 @@ router.get('/',
         res.json({ message: 'hooray! welcome to our API!' });
 });
 
+router.route('/movies')
+    .post(function ( req, res ) {
+
+        var movie = new Movie();
+
+        movie.name = req.body.name;
+
+        movie.save(function ( err ) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: 'Movie created!' });
+        });
+
+    })
+    .get(function (req, res) {
+        Movie.find(function (err, movies) {
+            if (err)
+                res.send(err);
+        
+            res.json(movies);
+
+        });
+});
+
+router.route('/movies/:movie_id')
+    .get(function(req, res) {
+        Movie.findById(req.params.movie_id,
+            function(err, movie) {
+                if (err)
+                    res.send(err);
+
+                res.json(movie);
+
+            });
+    })
+
+    .put(function(req, res) {
+
+        Movie.findById(req.params.movie_id, function (err, movie) {
+            if (err)
+                res.send(err);
+
+            console.log(req.body);
+            movie.name = req.body.name;
+
+
+            movie.save(function(err) {
+                if (err)
+                    res.send(err);
+
+                res.json({ message: 'Movie has been updated. Title is now ' + req.body.name });
+            });
+
+        });
+
+    })
+
+    .delete(function(req, res) {
+        Movie.remove({
+            _id: req.params.movie_id
+        }, function(err, movie) {
+            if (err)
+                res.send(err);
+
+            res.json({ message: req.params.movie_id + 'successfully deleted' });
+
+        });
+    });
+
+
 app.use('/api', router);
 
-//app.listen(port);
+app.listen(port);
 console.log('Magic happens on port ' + port);
+
+
 
 
