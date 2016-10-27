d36ef0eb03ea235a7ffb6950e0637023f73a0015
Preston (desktop) <smith.preston2@gmail.com>
2016-10-27 14:43:32 -0600

    Setup angular routes

---
+++ client/app/core/core.controller.js
@@ -0,0 +1,18 @@
+﻿(function() {
+    'use strict';
+
+    angular
+        .module('app.core')
+        .controller("coreController", coreController);
+
+    console.log('Inject the scope');
+    coreController.$inject = ['$scope'];
+
+    function coreController($scope) {
+
+        console.log('Adding the tagline');
+        $scope.tagline = 'To the moon and back';
+
+    }
+
+})();
