2a0c6ca1481539d94378837ad27899fdf0a73ffc
Preston (desktop) <smith.preston2@gmail.com>
2016-10-31 13:58:50 -0600

    Add page template and style it

--- client/app/about/about.controller.js
+++ client/app/about/about.controller.js
@@ -13,8 +13,11 @@
         var vm = this;
         vm.page = {
             name: '',
-            content: '',
-            title: ''
+            summary: {
+                title: '',
+                content: ''
+            },
+            title: '',
         }
 
         initialize();
@@ -25,14 +28,17 @@
         }
         
         function addTitle() {
-            vm.page.name = ' About ';
+            vm.page.name = 'About';
             vm.page.title = 'Preston';
         }
 
         function addContent() {
-            vm.page.content =
+            vm.page.summary.title = 'Software Engineer';
+            vm.page.summary.content =
                 ' Hi, Im Preston.' +
-                ' Sometimes I rhyme fast, sometimes I rhyme slow. ' + 
+                '\n' + '\n' +
+                ' Sometimes I rhyme fast, sometimes I rhyme slow. ' +
+                '\n' + '\n' +
                 ' Enient.Atiorerias aut aut ut pores nobitas nossequ aectatur, sequisint.' +
                 ' Ullitatur solutas ea quibeati rerum ea nosam net aut iur aciae officidis' +
                 ' re, ommodicabo.Us rempor repra volupis is milluptate veniend itatis cullace' +
