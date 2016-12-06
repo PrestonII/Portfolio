949a3387a20762b854d9dedb8faf3bcfa0cf9508
Preston (desktop) <smith.preston2@gmail.com>
2016-12-06 12:28:40 -0600

    add work page colors

--- client/app/app.router.js
+++ client/app/app.router.js
@@ -16,9 +16,29 @@
         });
 
         $routeProvider
-            .when('/', { templateUrl: 'client/app/about/about.view.html', controller: 'aboutController', controllerAs: 'vm'})
-            .when('/Works', { templateUrl: 'client/app/project/project.view.html', controller: 'projectController', controllerAs: 'vm'})
-            .when('/news', { templateUrl: 'client/app/app.view.html', controller: 'newsController', controllerAs: 'vm'})
+
+            // home + about pages
+            .when('/',{
+                templateUrl: 'client/app/about/about.view.html',
+                controller: 'aboutController',
+                controllerAs: 'vm'
+            })
+
+            // work pages
+            .when('/Works', {
+                templateUrl: 'client/app/project/project.view.html',
+                controller: 'projectController',
+                controllerAs: 'vm'
+            })
+
+            // news page
+            .when('/news', {
+                templateUrl: 'client/app/app.view.html',
+                controller: 'newsController',
+                controllerAs: 'vm'
+            })
+
+            // default - return to home
             .otherwise('/');
     }
 })();
