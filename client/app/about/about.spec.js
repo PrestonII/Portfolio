17143f1562464d316347017b0c621863d548c969
Preston (desktop) <smith.preston2@gmail.com>
2016-12-14 09:41:41 -0600

    modified project structure

---
+++ client/app/about/about.spec.js
@@ -0,0 +1,24 @@
+﻿// Jasmine Test
+describe('Jasmine - About Module Test', function() {
+    before
+})
+
+
+// Mocha Test
+
+//describe('About Module Tests',
+//    function () {
+
+//        beforeEach(module('app.about'));
+
+//        var About;
+
+//        beforeEach(inject(function(about)
+//        {
+//            About = about;
+//        }));
+
+//        it('Confirms that the about controller exists', function () {
+//            expect(About).to.not.be.undefined;
+//        });
+//    });
