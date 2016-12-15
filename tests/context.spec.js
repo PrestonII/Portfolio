41f6b730e2aabdde76e0a0b890ebb3fa602c07c2
Preston (desktop) <smith.preston2@gmail.com>
2016-12-15 10:32:51 -0600

    Navigator tests

---
+++ tests/context.spec.js
@@ -0,0 +1,60 @@
+﻿describe('Context', function () {
+    var Context;
+
+    // Before each test load our api.users module
+    beforeEach(angular.mock.module('app.core'));
+
+    // Before each test set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function (_context_) {
+        Context = _context_;
+    }));
+
+    describe('on creation',
+        function() {
+            // A simple test to verify the Project Controller exists
+            it('should exist',
+                function() {
+                    expect(Context).toBeDefined();
+                });
+
+            // A simple test to verify the Users factory exists
+            it('should not be undefined',
+                function() {
+                    expect(Context).not.toBeUndefined();
+                });
+
+            // A simple test to verify the method all exists
+            it('should be have an array called "currentPage"',
+                function () {
+                    expect(Context.currentPage).toBeDefined();
+                    //expect(Navigator.points).toBe('testing');
+                });
+        });
+
+    // A set of tests for our Users.all() method
+    describe('.updatePage()',
+        function() {
+            // A simple test to verify the method all exists
+            it('should exist',
+                function() {
+                    expect(Context.updatePage).toBeDefined();
+                });
+
+
+
+
+            //// A test to verify that calling all() returns the array of users we hard-coded above
+            //it('should return a hard-coded list of users', function () {
+            //    expect(Users.all()).toEqual(userList);
+            //});
+        });
+
+    //// A set of tests for our Users.findById() method
+    //describe('.findById()', function () {
+    //    // A simple test to verify the method findById exists
+    //    it('should exist', function () {
+    //        expect(Users.findById).toBeDefined();
+    //    });
+    //});
+
+});
---
+++ tests/navigator.spec.js
@@ -0,0 +1,53 @@
+﻿describe('Navigator', function () {
+    var Navigator;
+
+    // Before each test load our api.users module
+    beforeEach(angular.mock.module('app.core'));
+
+    // Before each test set our injected Users factory (_Users_) to our local Users variable
+    beforeEach(inject(function (_navigator_) {
+        Navigator = _navigator_;
+    }));
+
+    describe('on creation',
+        function() {
+            // A simple test to verify the Project Controller exists
+            it('should exist',
+                function() {
+                    expect(Navigator).toBeDefined();
+                });
+
+            // A simple test to verify the Users factory exists
+            it('should not be undefined',
+                function() {
+                    expect(Navigator).not.toBeUndefined();
+                });
+        });
+
+    // A set of tests for our Users.all() method
+    describe('.toggleMenu()',
+        function() {
+            // A simple test to verify the method all exists
+            it('should exist',
+                function() {
+                    expect(Navigator.toggleMenu).toBeDefined();
+                });
+
+
+
+
+            //// A test to verify that calling all() returns the array of users we hard-coded above
+            //it('should return a hard-coded list of users', function () {
+            //    expect(Users.all()).toEqual(userList);
+            //});
+        });
+
+    //// A set of tests for our Users.findById() method
+    //describe('.findById()', function () {
+    //    // A simple test to verify the method findById exists
+    //    it('should exist', function () {
+    //        expect(Users.findById).toBeDefined();
+    //    });
+    //});
+
+});
