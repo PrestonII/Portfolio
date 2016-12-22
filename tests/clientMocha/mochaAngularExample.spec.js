2621f2966846935efee0c81d069bd77f50c4017f
Preston (desktop) <smith.preston2@gmail.com>
2016-12-22 12:16:49 -0600

    Create mocha version of current Context tests

--- tests/clientMocha/mochaAngularExample.spec.js
+++ tests/clientMocha/mochaAngularExample.spec.js
@@ -1,5 +1,12 @@
-var assert = require('chai').assert;
-require('./testhelpers');
+var chai = require('chai');
+var sinon_chai = require('sinon-chai');
+var chai_promises = require('chai-as-promised');
+var assert = chai.assert;
+var expect = chai.expect;
+chai.use(sinon_chai);
+chai.use(chai_promises);
+
+require('./testvariables');
 
 require('../../client/app/core/core.app');
 require('../../client/app/core/core.context');
@@ -20,7 +27,8 @@ describe('Mocha + Angular Example', function () {
         function() {
             it('should log the message "something done!"',
                 function() {
-                    assert.ok(1 === 1, 'Fake Passing Test!');
+                    // assert.ok(1 === 1, 'Fake Passing Test!');
+                    expect(Context).to.not.be.undefined;
                 });
         });
 
