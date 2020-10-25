b904996edca3b3975041f92eb18df881b2e14ced
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-25 19:17:46 -0400

    cover images show up now too

--- components/post/PostSection.tsx
+++ components/post/PostSection.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import PageLink from '../PageLink';
 import ROUTES from '../Routes';
+import PostCoverImage from './PostCoverImage';
 import PostBody from './PostBody';
 import styles from './post.module.scss';
 
@@ -13,20 +14,26 @@ interface IPostSection {
   sectionNext?: string;
 }
 
-const PostSection = ({title, sectionTitle, sectionContent}: IPostSection) => (
-  <div className={styles.post}>
-    <div className={styles.post__title}>
-      <h2>{title}</h2>
-    </div>
-    <div className={styles.post__jumbotron}></div>
-    <div className={styles.post__summary}>
-      <div className={styles.post__summary__header}>
-        <h2>{sectionTitle}</h2>
-        <h4>hlw</h4>
+const PostSection = ({title, sectionTitle, sectionContent, coverImage, sectionNext}: IPostSection) => {
+  const imageUrl = coverImage ?? '';
+
+  return (
+    <div className={styles.post}>
+      <div className={styles.post__title}>
+        <h2>{title}</h2>
       </div>
-      <PostBody content={sectionContent} />
+      <div className={styles.post__jumbotron}>
+        <PostCoverImage title={title} src={imageUrl} slug={sectionNext} />
+      </div>
+      <div className={styles.post__summary}>
+        <div className={styles.post__summary__header}>
+          <h2>{sectionTitle}</h2>
+          <h4>hlw</h4>
+        </div>
+        <PostBody content={sectionContent} />
+      </div>
     </div>
-  </div>
-)
+  )
+}
 
 export default PostSection;
