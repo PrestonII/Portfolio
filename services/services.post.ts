5664bb7d31c1ae7f30ac3f756bbcc049d58e1099
Preston Smith <PrestonII@users.noreply.github.com>
2020-10-25 11:29:44 -0400

    moved fileServices to their own directory

--- services/services.post.ts
+++ services/services.post.ts
@@ -1,31 +1,10 @@
-import fs from 'fs';
 import * as path from 'path';
 import matter from 'gray-matter';
+import { getAllFiles } from './services.files';
 
 const postsDirectory = path.join(process.cwd(), '__content__');
 
 export function getAllPostSlugs() {
-  return getPostSlugs(postsDirectory);
+  return getAllFiles(postsDirectory);
 }
 
-export function getPostSlugs(baseDir: string): string[] {
-  let files: string[] = [];
-
-  fs
-    .readdirSync(baseDir)
-    .forEach(file => {
-      const fileName = path.join(baseDir, file);
-      
-      if(isDirectory(fileName)) {
-        files.push(...getPostSlugs(fileName))
-      } else {
-        files.push(fileName);
-      }
-    })
-
-  return files;
-}
-
-export function isDirectory(fileName: string): boolean {
-  return fs.statSync(fileName).isDirectory();
-}
