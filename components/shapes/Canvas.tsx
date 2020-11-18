644cc289ed3c4bb955091c085c6b1be03b42a984
Preston Smith <smith.preston2@gmail.com>
2020-11-18 07:55:28 -0500

    linked resizing to canvas stage size

--- components/shapes/Canvas.tsx
+++ components/shapes/Canvas.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef, useEffect } from 'react';
 import dynamic from 'next/dynamic';
 import Two from 'two.js';
-
+import styles from './Canvas.module.scss';
 
 // export default DynamicTwo;
 
@@ -19,27 +19,33 @@ const TwoComponent = (props: ITwoComp ) => {
   const two = useRef(new Two(params));
   const lib = Two.Instances
   const [shapes, setShapes] = React.useState(lib);
-  const stage = document.querySelectorAll('.stage')[0];
 
-  useEffect(setup, []);
+  useEffect(setup, [shapes]);
 
   
   function resize() {
     // Resize logic here
-    
+    const stage = document.querySelectorAll('.stage')[0];
+    console.log('resizing');
+    console.log(`Stage is: ${stage.clientWidth} by ${stage.clientHeight}`);
+    two.current.width = stage.clientWidth;
+    two.current.height = stage.clientHeight;
   }
 
   function update() {
     // animate shapes here
+    console.log('updating');
   }
 
   function setup() {
-
+    const stage = document.querySelectorAll('.stage')[0];
     two.current.appendTo(stage);
+    two.current.width = stage.clientWidth;
+    two.current.height = stage.clientHeight;
 
     // Add any shapes you'd like here
     
-    const shape = two.current.makeCircle(0, 0, 250);
+    const shape = two.current.makeCircle(500, 0, 250);
     two.current.add(shape);
 
     two.current.bind('update', update);
@@ -53,7 +59,7 @@ const TwoComponent = (props: ITwoComp ) => {
   }
 
   return (
-    <div className="stage" ref={ ref } />
+    <div className={`${styles.twoStage} stage`} ref={ ref } />
   )
 }
 
