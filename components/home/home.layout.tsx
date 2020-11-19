888ba554717e0c3424817d1ec94575392615ec07
Preston Smith <smith.preston2@gmail.com>
2020-11-19 16:51:39 -0500

    added three and imported as-is

--- components/home/home.layout.tsx
+++ components/home/home.layout.tsx
@@ -2,15 +2,17 @@ import React from 'react';
 
 import Layout from '../Layout'
 import { Home } from './home.component';
+import ThreeGuy from '../canvas/canvas.three';
 
 export default function HomeLayout() {
   const title = "FullStack Designer & Developer"
 
   return (
     <Layout title={`Preston Smith | ${title}`}>
+      <ThreeGuy />
+      {/* <Home title={title}/>
       <Home title={title}/>
-      <Home title={title}/>
-      <Home title={title}/>
+      <Home title={title}/> */}
     </Layout>
   )
 }
