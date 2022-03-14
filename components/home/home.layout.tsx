57efc029f3698d5bfe96c1ab52ab8d87d91330e3
Preston Smith <PrestonII@users.noreply.github.com>
2022-03-14 19:03:56 -0400

    update hover state

--- components/home/home.layout.tsx
+++ components/home/home.layout.tsx
@@ -5,10 +5,19 @@ import styles from './home.module.scss'
 
 export default function HomeLayout(): JSX.Element {
   const title = "FullStack Designer & Developer"
-  // const heading = "A Developer that can also Design products"
+  const headingText1 = (
+    <div className="">
+      A <span>Developer</span> that also <span>Designs</span> and <span>Builds</span>
+    </div>
+  )
+  const headingText2 = (
+    <div className="">
+      A <span>Developer</span> that can also <span>Design</span> product
+    </div>
+  )
   const heading = (
     <div className={styles.home__title}>
-      A <span>Developer</span> that can also <span>Design</span> products
+      { headingText1}
     </div>
   )
 
