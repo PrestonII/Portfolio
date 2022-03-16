32fb19eda93a2079dc1544c745b9787d79969d15
Preston Smith <PrestonII@users.noreply.github.com>
2022-03-16 17:49:02 -0400

    add modal

--- components/PageLink.tsx
+++ components/PageLink.tsx
@@ -1,31 +1,62 @@
-import React, { useEffect } from 'react';
-import Link from 'next/link';
-import { TimelineMax as Timeline, Power1, gsap } from 'gsap';
+import React, { useCallback } from 'react';
+// import Link from 'next/link';
+import { Button } from 'carbon-components-react';
+import { useRouter } from 'next/router';
 import styles from './pagelink.module.scss';
-import  OverflowHiddenParagraph, { ContainerProps, getWrapper } from './containers/container.hidden';
-import { IconArrow } from './icons/Icon.arrow';
+import OverflowHiddenParagraph, {
+  ContainerProps,
+  getWrapper,
+} from './containers/container.hidden';
+import { IconArrow } from './icons/Icon.Arrow';
 
 interface PageLinkProps extends ContainerProps {
   route: string;
+  onClick?: () => void;
+  preventProceeding?: boolean;
 }
 
 export const ExternalLink: React.FC<PageLinkProps> = (props) => (
   <div className={`${styles.pagelink}`}>
-    { getWrapper({containerType: 'anchor', children: props.children, wrapperRef: props.wrapperRef, linkSrc: props.route})}
+    {getWrapper({
+      containerType: 'anchor',
+      children: props.children,
+      wrapperRef: props.wrapperRef,
+      linkSrc: props.route,
+    })}
   </div>
 );
 
-export const InternalLink: React.FC<Omit<PageLinkProps, 'containerType'>> = (props) => {
+export const InternalLink: React.FC<Omit<PageLinkProps, 'containerType'>> = (
+  props,
+) => {
+  const router = useRouter();
   const outline = React.createRef<HTMLDivElement>();
 
+  const onClick = useCallback(() => {
+    props.onClick && props.onClick();
+    if (!props.preventProceeding) router.push(props.route);
+  }, [props.preventProceeding]);
+
   return (
-    <div ref={outline} className={`${styles.link} ${props.classOverrides}`} style={props.style} >
-      <Link href={props.route}>
-        <div className={styles.link__inner}>
-          <OverflowHiddenParagraph containerType='paragraph' wrapperRef={props.wrapperRef} classOverrides={styles.pagelink}>{props.children}</OverflowHiddenParagraph>
-          <IconArrow direction='NE' />
-        </div>
-      </Link>
+    <div
+      ref={outline}
+      className={`${styles.link} ${props.classOverrides}`}
+      style={props.style}
+      onClick={onClick}
+    >
+      <div
+        className={styles.link__inner}
+        onClick={() => console.log('clicked')}
+      >
+        <OverflowHiddenParagraph
+          containerType="paragraph"
+          wrapperRef={props.wrapperRef}
+          classOverrides={styles.pagelink}
+        >
+          {props.children}
+        </OverflowHiddenParagraph>
+        <IconArrow direction="NE" />
+      </div>
     </div>
   );
 };
