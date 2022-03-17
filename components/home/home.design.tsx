c9186ed0359bce0325ec6c78169cc9dcdfd1b4b1
Preston Smith <PrestonII@users.noreply.github.com>
2022-03-17 15:31:31 -0400

    extract pageheader

--- components/home/home.design.tsx
+++ components/home/home.design.tsx
@@ -1,14 +1,13 @@
-import OverflowHiddenContainer from '../containers/container.hidden';
-import FacetTitle from '../facets/facet.title';
-import { InternalLink } from '../PageLink';
 import ProjectStub from '../project/project.stub';
 import { ReactComponent as IconNimble } from '../assets/Icon_Nimble.svg';
 import { ReactComponent as IconPG } from '../assets/Icon_PG.svg';
 import { ReactComponent as IconPiper } from '../assets/Icon_Piper.svg';
 import { ReactComponent as IconDesign } from '../assets/ICON_DESIGN.svg';
 import styles from './subhome.module.scss';
+import PageHeader from '../page/PageHeader';
 
 const DesignHome: React.FC = () => {
+  const headerType = 'Design';
   const title = `Things I've Designed`;
   const msg = `I like working through the process of thinking about how things will look & feel. The idea of aesthetics as a semantic language is something that continues to inspire me.`;
   const nimble = {
@@ -39,21 +38,17 @@ const DesignHome: React.FC = () => {
   return (
     <div className={styles.subhome}>
       <div className={styles.subhome__wrapper}>
-        <div className={styles.subhome__title}>
-          <FacetTitle title="Design" icon={<IconDesign />} />
-          <OverflowHiddenContainer containerType="h2">
-            {title}
-          </OverflowHiddenContainer>
-          <OverflowHiddenContainer containerType="paragraph">
-            {msg}
-          </OverflowHiddenContainer>
-          <InternalLink
-            route="/work/design"
-            classOverrides={styles.subhome__link}
-          >
-            Thoughts On Design
-          </InternalLink>
-        </div>
+        <PageHeader
+          {...{
+            title,
+            msg,
+            headerType,
+            icon: <IconDesign />,
+            linkText: 'Thoughts On Design',
+            linkClassOverrides: styles.subhome__link,
+            classOverrides: styles.subhome__title,
+          }}
+        />
         <div className={styles.subhome__projects}>
           <ProjectStub {...nimble} userHasAccess />
           <ProjectStub {...program_generator} />
--- components/home/home.develop.tsx
+++ components/home/home.develop.tsx
@@ -1,14 +1,13 @@
-import OverflowHiddenContainer from '../containers/container.hidden';
-import FacetTitle from '../facets/facet.title';
-import { InternalLink } from '../PageLink';
 import ProjectStub from '../project/project.stub';
 import { ReactComponent as IconNimble } from '../assets/Icon_Nimble_Dev.svg';
 import { ReactComponent as IconPG } from '../assets/Icon_PG_Dev.svg';
 import { ReactComponent as IconTether } from '../assets/Icon_Tether_Dev.svg';
 import { ReactComponent as IconDev } from '../assets/ICON_DEV.svg';
 import styles from './subhome.module.scss';
+import PageHeader from '../page/PageHeader';
 
 const DevelopHome: React.FC = () => {
+  const headerType = 'Development';
   const title = `Things I've Developed`;
   const msg = `A big part of my job is wiring things together with code. I love using digital tools to compose all the things I (or others) have designed. It's another way I can explore how things work and bring them together.`;
   const nimble = {
@@ -39,21 +38,17 @@ const DevelopHome: React.FC = () => {
   return (
     <div className={styles.subhome}>
       <div className={styles.subhome__wrapper}>
-        <div className={styles.subhome__title}>
-          <FacetTitle title="Development" icon={<IconDev />} />
-          <OverflowHiddenContainer containerType="h2">
-            {title}
-          </OverflowHiddenContainer>
-          <OverflowHiddenContainer containerType="paragraph">
-            {msg}
-          </OverflowHiddenContainer>
-          <InternalLink
-            route="/work/design"
-            classOverrides={styles.subhome__link}
-          >
-            Thoughts On Development
-          </InternalLink>
-        </div>
+        <PageHeader
+          {...{
+            title,
+            msg,
+            headerType,
+            icon: <IconDev />,
+            linkText: 'Thoughts On Development',
+            linkClassOverrides: styles.subhome__link,
+            classOverrides: styles.subhome__title,
+          }}
+        />
         <div className={styles.subhome__projects}>
           <ProjectStub {...nimble} userHasAccess />
           <ProjectStub {...program_generator} />
