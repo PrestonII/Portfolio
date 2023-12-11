312a5f85a2fe3b6240217df2a9be25bd1033f693
Preston Smith <smith.preston2@gmail.com>
2023-12-11 11:33:31 -0500

    update work page layout

--- components/work/WorkDetail.tsx
+++ components/work/WorkDetail.tsx
@@ -1,22 +1,107 @@
 import React from 'react';
 import Layout from '../Layout';
-import PostSection from '../post/PostSection';
 import { PostProps } from '../../types/post';
+import Link from 'next/link';
 
 const WorkDetailPage = (props: PostProps) => {
-  const { title, content, date, coverImage } = props.post;
+  const {
+    title,
+    content,
+    date,
+    coverImage,
+    sectionTitle,
+    stack,
+    year,
+    client,
+  } = props.post;
+
+  console.log('content', content);
 
   return (
     <Layout title={`Preston Smith | ${title}`} navMethodList={{}}>
-      <PostSection
-        title={title}
-        sectionTitle="Now"
-        sectionContent={content}
-        date={date}
-        coverImage={coverImage}
-      />
+      <div
+        style={{
+          border: '1px solid black',
+          display: 'grid',
+          gridTemplateColumns: '1fr 1fr 1fr',
+          gridTemplateRows: '1fr 1fr 1fr',
+          height: '100vh',
+          marginLeft: 160,
+        }}
+      >
+        <div
+          style={{
+            border: '1px solid red',
+            gridColumn: '1 / 2',
+            gridRow: '1 / 3',
+          }}
+        >
+          <span>{title}</span>
+          <h2>{sectionTitle}</h2>
+          <Link href="/">Back</Link>
+        </div>
+        <div
+          style={{
+            border: '1px solid blue',
+            gridColumn: '2 / 4',
+            gridRow: '1 / 3',
+          }}
+        >
+          carousel
+        </div>
+        <div
+          style={{
+            border: '1px solid green',
+            gridColumn: '1 / 4',
+            gridRow: '3 / 4',
+            display: 'grid',
+            gridTemplateColumns: '1fr 1fr 1fr',
+          }}
+        >
+          <ProjectDetails client={client} year={year} stack={stack} />
+          <div
+            style={{
+              gridColumn: '2 / 4',
+              display: 'grid',
+              gridTemplateColumns: '1fr 1fr ',
+            }}
+          >
+            <div
+              dangerouslySetInnerHTML={{
+                __html: content,
+              }}
+              style={{
+                display: 'flex',
+                flexDirection: 'column',
+                justifyContent: 'center',
+              }}
+            />
+          </div>
+        </div>
+      </div>
     </Layout>
   );
 };
+
+const ProjectDetails = ({
+  client,
+  year,
+  stack,
+}: {
+  client?: string;
+  year?: string;
+  stack?: string[];
+}) => (
+  <div>
+    <h5>Client</h5>
+    <p>{client}</p>
+    <h5>Year</h5>
+    <p>{year}</p>
+    <h5>Stack</h5>
+    {stack?.map((item, index) => (
+      <p key={`${index}-${item}`}>{item}</p>
+    ))}
+  </div>
+);
 
 export default WorkDetailPage;
--- types/post.ts
+++ types/post.ts
@@ -1,44 +1,55 @@
 export interface IPostProps {
   fileContents: string;
   postName: string;
-  fields?: string[]
+  fields?: string[];
 }
 
 export interface IPostData {
-  slug: string
-  title: string
-  date: string
-  coverImage: string
-  excerpt?: string
+  slug: string;
+  title: string;
+  date: string;
+  coverImage: string;
+  excerpt?: string;
   ogImage?: {
-    url: string
-  }
-  content: string
+    url: string;
+  };
+  content: string;
 }
 
 export class PostData implements IPostData {
   title = '';
   coverImage = '';
   excerpt?: string | undefined;
-  ogImage?: { url: string; } | undefined;
+  ogImage?: { url: string } | undefined;
   slug = '';
   content = '';
   date: string = new Date().toString();
   // extraData: Record<string, unknown> = {};
+  sectionTitle?: string;
+  images?: string[];
+  client?: string;
+  year?: string;
+  stack?: string[];
 }
 
 export const PostFields = {
   Title: 'title',
+  SectionTitle: 'sectionTitle',
+  Excerpt: 'excerpt',
   Date: 'date',
   Slug: 'slug',
   Content: 'content',
-  Cover: 'coverImage'
-}
+  Cover: 'coverImage',
+  Images: 'images',
+  Client: 'client',
+  Year: 'year',
+  Stack: 'stack',
+};
 
 export type PostType = PostData;
 
 export type PostProps = {
-  post: PostType
-  morePosts: PostType[]
-  preview?: boolean
-}
+  post: PostType;
+  morePosts: PostType[];
+  preview?: boolean;
+};
