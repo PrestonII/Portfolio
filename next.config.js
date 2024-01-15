2297f0f219933fbcb4ad6ab06dada5f8ec97a50a
Preston Smith <smith.preston2@gmail.com>
2024-01-15 12:09:00 -0500

    fix module location warning

--- next.config.js
+++ next.config.js
@@ -1,7 +1,14 @@
+const path = require('path');
+
 const nextConfig = {
   /* config options here */
   // ignoreBuildErrors: true,
   removeConsole: false,
+  // sassOptions: {
+  //   includePaths: [
+  //     path.join(__dirname, 'node_modules', 'carbon-components', 'scss'),
+  //   ],
+  // },
 };
 
 module.exports = nextConfig;
