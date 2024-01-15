c71a5ce0228925ef00dcc3156ad4a056fc412199
Preston Smith <smith.preston2@gmail.com>
2024-01-15 10:31:03 -0500

    Revert "removed ibm web components"
    This reverts commit 6ff77f037d82c89d369eaa8a991ee6ce235fed85.

--- components/work/WorkDetail.tsx
+++ components/work/WorkDetail.tsx
@@ -4,6 +4,7 @@ import { PostProps } from '../../types/post';
 import Link from 'next/link';
 import { CldImage } from 'next-cloudinary';
 import useResizeObserver from 'use-resize-observer';
+import Carousel from '@carbon/ibmdotcom-web-components/es/components-react/carousel/carousel';
 
 const WorkDetailPage = (props: PostProps) => {
   const [imageWidth, setImageWidth] = useState<number>(100);
@@ -59,7 +60,25 @@ const WorkDetailPage = (props: PostProps) => {
             gridRow: '1 / 3',
           }}
           ref={ref}
-        ></div>
+        >
+          <Carousel pageSize={1}>
+            <CldImage
+              src="cld-sample-5"
+              width={`${imageWidth}`}
+              height={`${imageHeight}`}
+            />
+            <CldImage
+              src="cld-sample-4"
+              width={`${imageWidth}`}
+              height={`${imageHeight}`}
+            />
+            <CldImage
+              src="cld-sample-3"
+              width={`${imageWidth}`}
+              height={`${imageHeight}`}
+            />
+          </Carousel>
+        </div>
         <div
           style={{
             border: '1px solid green',
