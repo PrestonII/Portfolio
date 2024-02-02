39dd0ac2f375c63c8abf02c8f916c796a42920c6
Preston Smith <smith.preston2@gmail.com>
2024-02-02 16:48:48 -0500

    allow nextjs to build even w/ unused code

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const path = require('path');
 
 const nextConfig = {
   /* config options here */
-  // ignoreBuildErrors: true,
+  ignoreBuildErrors: true,
   removeConsole: false,
   // sassOptions: {
   //   includePaths: [
